09f77f8ca7671997756ecbd7c917d656b2c4a006
Homicek <filip.sourek1@tul.cz>
2024-09-26 15:38:46 +0200

    #3

--- MyApplication002/app/src/main/java/com/example/myapplication002/MainActivity.kt
+++ MyApplication002/app/src/main/java/com/example/myapplication002/MainActivity.kt
@@ -1,20 +1,62 @@
 package com.example.myapplication002
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.annotation.SuppressLint
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
+
+         /*
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+         }
+         */
+
+        val etName = findViewById<EditText>(R.id.etName)
+        val etSurname = findViewById<EditText>(R.id.etSurname)
+        val etTown = findViewById<EditText>(R.id.etTown)
+        val etAge = findViewById<EditText>(R.id.etAge)
+        val twInformation = findViewById<TextView>(R.id.twInformation)
+        val btnSend = findViewById<EditText>(R.id.btnSend)
+        val btnDelete = findViewById<EditText>(R.id.btnVymazat)
+
+        //Nastavení pro tlačítko odeslat
+        btnSend.setOnClickListener {
+            val name = etName.text.toString()
+            val surname = etSurname.text.toString()
+            val town = etTown.text.toString()
+            val age = etAge.text.toString()
+
+            // Zobrazení textu v TextView
+            val formattedText = "Jmenuji se $name $surname, jsem z $town a je mi $age let."
+            twInformation.text = formattedText
         }
+
+        btnDelete.setOnClickListener {
+            etName.text.clear()
+            etSurname.text.clear()
+            etTown.text.clear()
+            etAge.text.clear()
+
+            twInformation.text = ""
+        }
+
+
+
+
     }
 }
