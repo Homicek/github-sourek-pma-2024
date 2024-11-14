31bb14e455ce5a7da84ea0da66c2e2d1b0eb1900
Homicek <filip.sourek1@tul.cz>
2024-11-14 11:10:09 +0100

    Gradle nastavení

---
+++ Poznamky00x/app/src/main/java/com/example/poznamky00x/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.poznamky00x
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.poznamky00x.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
